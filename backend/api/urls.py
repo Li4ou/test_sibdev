d4e47c37c43f9cd029faeac22925d1ed9d28d7e7
rochev <rochev_2015@mail.ru>
2021-07-31 14:26:26 +0300

    Добавление /

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from .views import CsvViewsFiles
 #TODO Написание эндпоинтов для Get и Post
 urlpatterns = [
-    path('/post', CsvViewsFiles.as_view({'post':'create'}))
+    path('post/', CsvViewsFiles.as_view({'post':'create'}))
 ]
