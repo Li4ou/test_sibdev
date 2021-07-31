399c152da96a67bdab318bac1b44a57f4d5c57f0
rochev <rochev_2015@mail.ru>
2021-07-31 13:29:43 +0300

    Добавление модели PurchaseHistory

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+#TODO Добавить class Meta
+class PurchaseHistory(models.Model):
+    """Покупки"""
+    customer = models.CharField("Логин", max_length=255)
+    product = models.CharField("Наименование товара", max_length=255)
+    total = models.DecimalField("Сумма сделки", max_digits=9, decimal_places=2)
+    quantity = models.DecimalField("Количество товара", max_digits=9, decimal_places=0)
+    date = models.DateTimeField("Время")
+
+    def __str__(self):
+        return self.customer
+
+
 # Create your models here.
