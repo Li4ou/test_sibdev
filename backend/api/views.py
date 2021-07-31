73fee853c8c619acb349d9403dff8dbec377db25
rochev <rochev_2015@mail.ru>
2021-07-31 14:08:51 +0300

    pass

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+
+from .serializer import SerializersPurchaseHistoryCreate
+
+
+# TODO Написание views для обработки фалов
+class CsvViewsFiles(viewsets.ModelViewSet):
+    """ """
+    serializer_class = SerializersPurchaseHistoryCreate
+
 
 # Create your views here.
