c772c6661ebcc64915c0e1bd43d6abe9d0ddeca9
rochev <rochev_2015@mail.ru>
2021-07-31 14:21:55 +0300

    Добавление view для оброботки file

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,13 +1,31 @@
 from django.shortcuts import render
+
+from rest_framework import status
 from rest_framework import viewsets
+from rest_framework.response import Response
 
 from .serializer import SerializersPurchaseHistoryCreate
-
-
-# TODO Написание views для обработки фалов
-class CsvViewsFiles(viewsets.ModelViewSet):
-    """ """
-    serializer_class = SerializersPurchaseHistoryCreate
+from .parsers import CSVTextParser
 
 
 # Create your views here.
+
+
+class CsvViewsFiles(viewsets.ModelViewSet):
+    parser_classes = [CSVTextParser]
+    serializer_class = SerializersPurchaseHistoryCreate
+
+    def create(self, request, *args, **kwargs):
+
+        serializer = self.get_serializer(data=request.data, many=isinstance(request.data, list))
+        serializer.is_valid()
+        if serializer.is_valid():
+            self.perform_create(serializer)
+            headers = self.get_success_headers(serializer.data)
+            return Response({'status': 'ok'}, status=status.HTTP_200_OK, headers=headers)
+
+        return Response({'Status': 'Error', 'Desc':serializer.errors}, status=status.HTTP_400_BAD_REQUEST )
+
+
+
+
