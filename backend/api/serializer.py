567c7707a6953351cca9e938dbb60cbf526e489b
rochev <rochev_2015@mail.ru>
2021-08-02 13:57:08 +0300

    Добавление PurchaseHistorySerializers

--- backend/api/serializer.py
+++ backend/api/serializer.py
@@ -1,8 +1,52 @@
+import logging
+
+from django.db.models import ObjectDoesNotExist
 from rest_framework import serializers
 
+from .models import Product
+from .models import Customer
 from .models import PurchaseHistory
 
-class SerializersPurchaseHistoryCreate(serializers.ModelSerializer):
+logger = logging.getLogger(__name__)
+
+
+class ProductSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['name']
+
+
+class CustomerSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Customer
+        fields = ['login', 'money_spent']
+
+
+class PurchaseHistorySerializers(serializers.ModelSerializer):
+    customer = CustomerSerializers()
+    item = ProductSerializers()
+
     class Meta:
         model = PurchaseHistory
         fields = ('customer', 'item', "total", 'quantity', 'date')
+
+    def create(self, validated_data: dict):
+
+        product_name = validated_data.pop('item')
+        customer_login: dict = validated_data.pop('customer')
+        try:
+            product = Product.objects.get(**product_name)
+        except ObjectDoesNotExist:
+            product = Product.objects.create(**product_name)
+
+        try:
+            customer = Customer.objects.get(login=customer_login['login'])
+            customer.money_spent += validated_data['total']
+            customer.save()
+        except ObjectDoesNotExist:
+            customer = Customer.objects.create(**customer_login)
+        if product is not customer.stone.all():
+            customer.stone.add(product)
+            customer.save()
+
+        return PurchaseHistory.objects.create(customer=customer, item=product, **validated_data)
