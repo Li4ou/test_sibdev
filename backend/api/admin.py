a4a5497b94e6a781b2efdc6c4392993ba7aa3d26
rochev <rochev_2015@mail.ru>
2021-08-03 14:44:27 +0300

    Регитсрация моделей в админ

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from .models import PurchaseHistory
+from .models import Customer
+from .models import Product
+
 # Register your models here.
+
+admin.site.register(PurchaseHistory)
+admin.site.register(Customer)
+admin.site.register(Product)
+
