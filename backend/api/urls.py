044d42d0c36cdc3c36de9d77caea9b2a08d51d1b
rochev <rochev_2015@mail.ru>
2021-08-03 14:37:40 +0300

    Эндпоинт для обработки Get

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
 from .views import CsvViewsFiles
-#TODO Написание эндпоинтов для Get и Post
+from .views import CustomerView
+
 urlpatterns = [
-    path('post/', CsvViewsFiles.as_view({'post':'create'}))
+    path('post/', CsvViewsFiles.as_view({'post': 'create'})),
+    path('get/', CustomerView.as_view())
 ]
